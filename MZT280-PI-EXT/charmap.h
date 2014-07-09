543863fb06938c7dd5277fef86be80d4a22a55b9
MrScrith <erik@trouserenthusiast.com>
2014-07-09 15:06:27 -0500

    Added a character map of unknown quality, will have to play around iwth it to figure out what is actually happening.

---
+++ MZT280-PI-EXT/charmap.h
@@ -0,0 +1,17 @@
+/* facename fixed
+ * copyright Public domain font.  Share and enjoy.
+ *
+ * height 13
+ * width 6
+ *
+ * Full layout of all 256 ASCII characters in 6x13 bit format. 
+ * Each caracter layout is an array of 13 shorts, with each 1 bit
+ * representing a forground pixel, and each 0 bit representing a background 
+ * pixel (forground and background colors being defined by the display at that
+ * time).
+ */
+ 
+ 
+
+
+extern const  char caracters[][13];
